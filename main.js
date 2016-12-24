c00144de81c0d973890c8f2f1fbecca68388aae2
Ivan Ivanov <kzv@narod.ru>
2016-12-24 19:02:52 +0000

    allow CORS

--- main.js
+++ main.js
@@ -33,7 +33,6 @@ app.use(function (req, res, next) {
 
     // Request headers you wish to allow
     //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type', 'Access-Control-Allow-Origin');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
