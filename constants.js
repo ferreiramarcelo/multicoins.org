7c26aa4ef6d9be70984b88185291f80628a5b237
Ivan Ivanov <kzv@narod.ru>
2017-02-28 05:16:38 +0000

    fix message verify

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 'use strict';
 
-exports.my_port = process.env.PORT; //8088;//8080; //8088; //process.env.PORT; //8088; //http port
+exports.my_port = process.env.PORT || 8088; //8088;//8080; //8088; //process.env.PORT; //8088; //http port
 exports.my_portSSL = 9443; //https port
-exports.my_domain = 'multicoins.org:9443'; //'multicoins-kzv.c9users.io:8080';//'multicoins.org:9443'; //'multicoins-kzv.c9users.io'; //'multicoins.org';
+exports.my_domain = 'multicoins.org:' + exports.my_portSSL; //'multicoins-kzv.c9users.io:8080';//'multicoins.org:9443'; //'multicoins-kzv.c9users.io'; //'multicoins.org';
