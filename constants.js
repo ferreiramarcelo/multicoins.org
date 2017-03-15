abd56ba35adccc2681d5ac43c057509a173ff4d4
Ivan Ivanov <kzv@narod.ru>
2017-03-15 10:24:45 +0000

    fix worked wallet

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 'use strict';
 
-exports.my_port = process.env.PORT; //8088; //http port
-exports.my_portSSL = 9443; //https port
+exports.my_port = process.env.PORT || 8088;//8080; //8088; //process.env.PORT; //8088; //http port
+exports.my_portSSL = 11443; //https port
